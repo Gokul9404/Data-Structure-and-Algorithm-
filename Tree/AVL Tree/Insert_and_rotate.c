064b2d73d3ebadbf5ee0211be185f5ead91758a5
Gokul <gokulgamer9404@gmail.com>
2022-02-07 22:05:00 +0530

    AVL Tree update

--- Tree/AVL Tree/Insert_and_rotate.c
+++ Tree/AVL Tree/Insert_and_rotate.c
@@ -5,15 +5,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 //---------------------------------------------------------------------------------------
-struct Node{
+struct Node
+{
     int key;
     struct Node *left;
     struct Node *right;
     int height;
 };
 
-struct Node *createNode(int key){
-    struct Node* node = (struct Node *) malloc(sizeof(struct Node));
+struct Node *createNode(int key)
+{
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
     node->key = key;
     node->left = NULL;
     node->right = NULL;
@@ -21,107 +23,125 @@ struct Node *createNode(int key){
     return node;
 }
 //---------------------------------------------------------------------------------------
-int max (int a, int b){
-    return (a>b)?a:b;
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
 }
- 
-int getHeight(struct Node *n){
-    if(n==NULL)
+
+int getHeight(struct Node *n)
+{
+    if (n == NULL)
         return 0;
     return n->height;
 }
 
-int getBalanceFactor(struct Node * n){
-    if(n==NULL){
+int getBalanceFactor(struct Node *n)
+{
+    if (n == NULL)
+    {
         return 0;
     }
     return getHeight(n->left) - getHeight(n->right);
 }
 //---------------------------------------------------------------------------------------
-struct Node* rightRotate(struct Node* y){
-    struct Node* x = y->left;
-    struct Node* T2 = x->right;
- 
+struct Node *rightRotate(struct Node *y)
+{
+    struct Node *x = y->left;
+    struct Node *T2 = x->right;
+
     x->right = y;
     y->left = T2;
- 
+
     x->height = max(getHeight(x->right), getHeight(x->left)) + 1;
     y->height = max(getHeight(y->right), getHeight(y->left)) + 1;
- 
+
     return x;
 }
- 
-struct Node* leftRotate(struct Node* x){
-    struct Node* y = x->right;
-    struct Node* T2 = y->left;
- 
+
+struct Node *leftRotate(struct Node *x)
+{
+    struct Node *y = x->right;
+    struct Node *T2 = y->left;
+
     y->left = x;
     x->right = T2;
- 
+
     x->height = max(getHeight(x->right), getHeight(x->left)) + 1;
     y->height = max(getHeight(y->right), getHeight(y->left)) + 1;
- 
+
     return y;
 }
 //---------------------------------------------------------------------------------------
-struct Node *insert(struct Node* node, int key){
+struct Node *insert(struct Node *node, int key)
+{
     if (node == NULL)
-        return  createNode(key);
-    
+        return createNode(key);
+
     if (key < node->key)
-        node->left  = insert(node->left, key);
+        node->left = insert(node->left, key);
     else if (key > node->key)
         node->right = insert(node->right, key);
- 
+
     node->height = 1 + max(getHeight(node->left), getHeight(node->right));
     int bf = getBalanceFactor(node);
     // Left Left Case
-    if(bf>1 && key < node->left->key){
+    if (bf > 1 && key < node->left->key)
+    {
         return rightRotate(node);
     }
     // Right Right Case
-    if(bf<-1 && key > node->right->key){
+    if (bf < -1 && key > node->right->key)
+    {
         return leftRotate(node);
     }
     // Left Right Case
-    if(bf>1 && key > node->left->key){
+    if (bf > 1 && key > node->left->key)
+    {
         node->left = leftRotate(node->left);
         return rightRotate(node);
     }
     // Right Left Case
-    if(bf<-1 && key < node->right->key){
+    if (bf < -1 && key < node->right->key)
+    {
         node->right = rightRotate(node->right);
         return leftRotate(node);
     }
     return node;
 }
 //---------------------------------------------------------------------------------------
-void InOrder(struct Node *root){
-    if(root != NULL){
+void InOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
         InOrder(root->left);
         printf("%d ", root->key);
         InOrder(root->right);
     }
 }
- 
-void PreOrder(struct Node *root){
-    if(root != NULL){
+
+void PreOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
         printf("%d ", root->key);
         PreOrder(root->left);
         PreOrder(root->right);
     }
 }
 
-void PostOrder(struct Node *root){
-    if(root != NULL){
+void PostOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
         PostOrder(root->left);
         PostOrder(root->right);
         printf("%d ", root->key);
     }
 }
 //---------------------------------------------------------------------------------------
-int main(){
-    struct Node * root = NULL;
+int main()
+{
+    struct Node *root = NULL;
     //-----------------------------------------------------------------------------------
     root = insert(root, 1);
     root = insert(root, 2);
@@ -140,7 +160,7 @@ int main(){
                     4
                 2       5
               1   3       6
-    
+
     */
     return 0;
 }
