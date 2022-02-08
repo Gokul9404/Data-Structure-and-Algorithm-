4183bfb40b3d3d7ccc3a11d15b5dd9ec776f79ec
Gokul <gokulgamer9404@gmail.com>
2022-02-08 20:04:00 +0530

    Updated README & requirement files

--- Tree/Binary Tree/basic.c
+++ Tree/Binary Tree/basic.c
@@ -1,54 +1,56 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Binary_node{
+struct Binary_node
+{
     int data;
-    struct Binary_node* left;
-    struct Binary_node* right;
+    struct Binary_node *left;
+    struct Binary_node *right;
 };
 
-
-struct Binary_node* Create_node(int dataa){
-    struct Binary_node* node;
-    node = (struct Binary_node *) malloc(sizeof(struct Binary_node));
+struct Binary_node *Create_node(int dataa)
+{
+    struct Binary_node *node;
+    node = (struct Binary_node *)malloc(sizeof(struct Binary_node));
     node->data = dataa;
     node->left = NULL;
     node->right = NULL;
     return node;
 }
 
-int main(){
+int main()
+{
     /*
     Here created binary tree is like below:-
-                    1 
+                    1
                 3       2
             5   7       4   6
     */
 
-    struct Binary_node* head;
+    struct Binary_node *head;
     head = Create_node(1);
 
-    struct Binary_node* left;
+    struct Binary_node *left;
     left = Create_node(3);
-    
-    struct Binary_node* left11;
+
+    struct Binary_node *left11;
     left11 = Create_node(5);
-    
-    struct Binary_node* left12;
+
+    struct Binary_node *left12;
     left12 = Create_node(7);
-    
-    struct Binary_node* right;
+
+    struct Binary_node *right;
     right = Create_node(2);
 
-    struct Binary_node* right11;
+    struct Binary_node *right11;
     right11 = Create_node(4);
-    
-    struct Binary_node* right12;
+
+    struct Binary_node *right12;
     right12 = Create_node(6);
 
     head->left = left;
     head->right = right;
-    
+
     left->left = left11;
     left->right = left12;
 
